b349279fcc60719fb95601905247b3b5b1124150
mszeredi <mszeredi>
2004-04-01 15:24:03 +0000

    fix

--- glassfs/glassfs.c
+++ glassfs/glassfs.c
@@ -11,6 +11,21 @@
 /* some random number */
 #define GLASSFS_MAGIC	0x28476c61
 
+#define AVFS_MAGIC_CHAR '#'
+#define OVERLAY_DIR "/overlay"
+#define OVERLAY_DIR_LEN 8
+
+struct base_entry {
+	struct dentry *dentry;
+	struct vfsmount *mnt;
+};
+
+/* Rules for storing underlying filesystem data:
+ *
+ *  dentry->d_fsdata:     contains the underlying dentry 
+ *  inode->u.generic_ip:  contains a kmalloced base_entry struct 
+ *  sb->s_fs_info:        contains the underlying superblock
+ */
 
 static struct super_operations glassfs_ops;
 static struct inode_operations glassfs_file_inode_operations;
@@ -19,25 +34,37 @@ static struct inode_operations glassfs_dir_inode_operations;
 static struct inode_operations glassfs_symlink_inode_operations;
 static struct dentry_operations glassfs_dentry_operations;
 
-static struct inode *glassfs_get_inode(struct super_block *sb, int mode, dev_t dev)
+static struct inode *glassfs_get_inode(struct super_block *sb, int mode,
+				       dev_t dev, struct dentry *ndentry,
+				       struct vfsmount *nmnt)
 {
+	struct base_entry *be;
 	struct inode * inode = new_inode(sb);
+	if (!inode)
+		return NULL;
+	
+	be = kmalloc(sizeof(struct base_entry), GFP_KERNEL);
+	if (!be) {
+		iput(inode);
+		return NULL;
+	}
 
-	if (inode) {
-		inode->i_mode = mode;
-		inode->i_fop = &glassfs_file_operations;
-
-		switch (mode & S_IFMT) {
-		default:
-			inode->i_op = &glassfs_file_inode_operations;
-			break;
-		case S_IFDIR:
-			inode->i_op = &glassfs_dir_inode_operations;
-			break;
-		case S_IFLNK:
-			inode->i_op = &glassfs_symlink_inode_operations;
-			break;
-		}
+	be->dentry = dget(ndentry);
+	be->mnt =  mntget(nmnt);
+	inode->u.generic_ip = be;
+	inode->i_mode = mode;
+	inode->i_fop = &glassfs_file_operations;
+	
+	switch (mode & S_IFMT) {
+	default:
+		inode->i_op = &glassfs_file_inode_operations;
+		break;
+	case S_IFDIR:
+		inode->i_op = &glassfs_dir_inode_operations;
+		break;
+	case S_IFLNK:
+		inode->i_op = &glassfs_symlink_inode_operations;
+		break;
 	}
 	return inode;
 }
@@ -70,8 +97,9 @@ static void exchange_files(struct file *file1, struct file *file2)
 
 static int glassfs_open(struct inode *inode, struct file *file)
 {
-	struct vfsmount *nmnt = mntget(file->f_vfsmnt->mnt_sb->s_fs_info);
-	struct dentry *ndentry = dget(file->f_dentry->d_fsdata);
+	struct base_entry *be = inode->u.generic_ip;
+	struct vfsmount *nmnt = mntget(be->mnt);
+	struct dentry *ndentry = dget(be->dentry);
 	struct file *nfile;
 //	printk("glassfs_open\n");
 	nfile = dentry_open(ndentry, nmnt, file->f_flags);
@@ -85,16 +113,15 @@ static int glassfs_open(struct inode *inode, struct file *file)
 static int glassfs_getattr(struct vfsmount *mnt, struct dentry *dentry,
 			   struct kstat *stat)
 {
-	struct vfsmount *nmnt = mnt->mnt_sb->s_fs_info;
-	struct dentry *ndentry = dentry->d_fsdata;
+	struct base_entry *be = dentry->d_inode->u.generic_ip;
 //	printk("glassfs_getattr\n");
-	return vfs_getattr(nmnt, ndentry, stat);
+	return vfs_getattr(be->mnt, be->dentry, stat);
 }
 
 static int is_avfs(const unsigned char *name, unsigned int len)
 {
 	for (; len--; name++)
-		if (*name == (unsigned char) '@')
+		if (*name == (unsigned char) AVFS_MAGIC_CHAR)
 			return 1;
 	return 0;
 }
@@ -112,38 +139,85 @@ static void print_path(struct dentry *dentry, struct nameidata *nd)
 		free_page((unsigned long) page);
 	}
 }
+#if 0
+static struct dentry *lookup_avfs(struct dentry *dentry, struct nameidata *nd)
+{
+	struct dentry *avfsdentry = NULL;
+	char *page;
+	char *path;
+	
+	page = (char *) __get_free_page(GFP_KERNEL);
+	if (page) {
+		int err;
+		struct nameidata avfsnd;
+		unsigned int offset = PAGE_SIZE - dentry->d_name.len - 1;
+		strncpy(page + offset, dentry->d_name.name, dentry->d_name.len);
+		page[PAGE_SIZE - 1] = '\0';
+		strcpy(page + PAGE_SIZE - dentry->d_name.len);
+		path = d_path(nd->dentry,nd->mnt, page, offset);
+		if(!IS_ERR(path) && page + OVERLAY_DIR_LEN < path) {
+			path -= OVERLAY_DIR_LEN;
+			memcpy(path, OVERLAY_DIR, OVERLAY_DIR_LEN);
+			
+			err = path_lookup(path, 0, &avfsnd);
+			if(!err) {
+				/* FIXME: check that mnt is really
+				   AVFS, and that we hold a reference
+				   to it somewhere else */
+				mntput(nd->mnt);
+				avfsdentry = nd->dentry;
+			}
+		}
 
+		err = path_lookup("/overlay", 0, &avfsnd);
+		if(!err) {
+			err = path_walk(path, &avfsnd);
+			if(!err) {
+				err = path_walk(
+			}
+		}
+		free_page((unsigned long) page);
+	}
+	return avfsdentry;
+}
+#endif
 
 static struct dentry *glassfs_lookup(struct inode *dir, struct dentry *dentry,
 				    struct nameidata *nd)
 {
-	struct dentry *ndir = dir->u.generic_ip;
+	struct base_entry *be = dir->u.generic_ip;
 	struct dentry *ndentry;
+	struct vfsmount *nmnt = be->mnt;
 	struct inode *inode = NULL;
 
 //	printk("glassfs_lookup\n");
-	down(&ndir->d_inode->i_sem);
-	ndentry = lookup_hash(&dentry->d_name, ndir);
-	up(&ndir->d_inode->i_sem);
+	down(&be->dentry->d_inode->i_sem);
+	ndentry = lookup_hash(&dentry->d_name, be->dentry);
+	up(&be->dentry->d_inode->i_sem);
 	if(nd && !ndentry->d_inode &&
 	   is_avfs(dentry->d_name.name, dentry->d_name.len)) {
+		print_path(dentry, nd);
+#if 0
 		struct dentry *avfsdentry;
-
+		int total_link_count_save = current->total_link_count;
 		avfsdentry = lookup_avfs(dentry, nd);
-		if(avfsdentry->d_inode) {
-			
-		}
+		current->total_link_count = total_link_count_save;
+		if(avfsdentry && avfsdentry->d_inode) {
+			dput(ndentry);
+			ndentry = avfsdentry;
+		} else
+			dput(avfsdentry);
+#endif
 	}
 
 	if(ndentry->d_inode) {
 		struct inode *ninode = ndentry->d_inode;
 		inode = glassfs_get_inode(dir->i_sb, ninode->i_mode,
-					  ninode->i_rdev);
+					  ninode->i_rdev, ndentry, nmnt);
 		if(!inode) {
 			dput(ndentry);
 			return ERR_PTR(-ENOMEM);
 		}
-		inode->u.generic_ip = dget(ndentry);
 	}
 	dentry->d_fsdata = ndentry;
 	dentry->d_op = &glassfs_dentry_operations;
@@ -154,8 +228,8 @@ static struct dentry *glassfs_lookup(struct inode *dir, struct dentry *dentry,
 static int glassfs_permission(struct inode *inode, int mask,
 			   struct nameidata *nd)
 {
-	struct dentry *ndentry = inode->u.generic_ip;
-	struct inode *ninode = ndentry->d_inode;
+	struct base_entry *be = inode->u.generic_ip;
+	struct inode *ninode = be->dentry->d_inode;
 //	printk("glassfs_permission\n");
 	return permission(ninode, mask, NULL);
 }
@@ -163,8 +237,7 @@ static int glassfs_permission(struct inode *inode, int mask,
 
 static int glassfs_statfs(struct super_block *sb, struct kstatfs *buf)
 {
-	struct vfsmount *nmnt = sb->s_fs_info;
-	struct super_block *nsb = nmnt->mnt_sb;
+	struct super_block *nsb = sb->s_fs_info;
 //	printk("glassfs_statfs\n");
 	return vfs_statfs(nsb, buf);
 }
@@ -222,8 +295,10 @@ static int glassfs_follow_link(struct dentry *dentry, struct nameidata *nd)
 
 static void glassfs_clear_inode(struct inode *inode)
 {
-	struct dentry *ndentry = inode->u.generic_ip;
-	dput(ndentry);
+	struct base_entry *be = inode->u.generic_ip;
+	dput(be->dentry);
+	mntput(be->mnt);
+	kfree(be);
 }
 
 static struct dentry_operations glassfs_dentry_operations = {
@@ -265,14 +340,14 @@ static int glassfs_fill_super(struct super_block * sb, void * data, int silent)
 {
 	struct inode * inode;
 	struct dentry * root;
-	struct vfsmount *nmnt = current->fs->rootmnt;
-	struct super_block *nsb = nmnt->mnt_sb;
+	struct super_block *nsb = current->fs->rootmnt->mnt_sb;
 
 	sb->s_blocksize = nsb->s_blocksize;
 	sb->s_blocksize_bits = nsb->s_blocksize_bits;
 	sb->s_magic = GLASSFS_MAGIC;
 	sb->s_op = &glassfs_ops;
-	inode = glassfs_get_inode(sb, S_IFDIR | 0755, 0);
+	inode = glassfs_get_inode(sb, S_IFDIR | 0755, 0, current->fs->root,
+				  current->fs->rootmnt);
 	if (!inode)
 		return -ENOMEM;
 
@@ -281,9 +356,9 @@ static int glassfs_fill_super(struct super_block * sb, void * data, int silent)
 		iput(inode);
 		return -ENOMEM;
 	}
-	sb->s_fs_info = mntget(nmnt);
+	sb->s_fs_info = nsb;
 	root->d_fsdata = dget(current->fs->root);
-	inode->u.generic_ip = current->fs->root;
+	root->d_op = &glassfs_dentry_operations;
 
 	sb->s_root = root;
 	return 0;
@@ -296,23 +371,23 @@ static struct super_block *glassfs_get_sb(struct file_system_type *fs_type,
 	return get_sb_nodev(fs_type, flags, data, glassfs_fill_super);
 }
 
+#if 0
 static void glassfs_kill_sb(struct super_block *sb)
 {
-	struct vfsmount *nmnt = sb->s_fs_info;
 	struct dentry *ndentry = sb->s_root->d_fsdata;
 	
 //	printk("glassfs_kill_sb\n");
 	dput(ndentry);
-	mntput(nmnt);
 
 	kill_anon_super(sb);
 }
+#endif
 
 static struct file_system_type glassfs_fs_type = {
 	.owner		= THIS_MODULE,
 	.name		= "glassfs",
 	.get_sb		= glassfs_get_sb,
-	.kill_sb	= glassfs_kill_sb,
+	.kill_sb	= kill_anon_super,
 };
 
 static int __init init_glassfs_fs(void)

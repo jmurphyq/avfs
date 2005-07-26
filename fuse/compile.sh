718c67c4640dc882a1b2bebc1cfe47ed6f912979
mszeredi <mszeredi>
2005-07-26 15:35:14 +0000

    fixup build of avfsd

--- fuse/compile.sh
+++ fuse/compile.sh
@@ -2,5 +2,5 @@
 
 #gcc -I../include -Wall avfsd.c -o avfsd ../lib/avfs.o -L../libneon -lneon -lxmltok -lxmlparse -ldl -lpthread -lfuse
 
-gcc -I../include -Wall -W -g -D_FILE_OFFSET_BITS=64 -D_REENTRANT -D_POSIX_PTHREAD_SEMANTICS avfsd.c -o avfsd ../lib/avfs.o -ldl -lfuse -lpthread
+gcc -I../include -Wall -W -g -D_FILE_OFFSET_BITS=64 -D_REENTRANT -D_POSIX_PTHREAD_SEMANTICS avfsd.c -o avfsd ../lib/.libs/libavfs.a -ldl -lfuse -lpthread
 

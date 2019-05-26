8989ae447eaea0a700ae25e72898fddfec6e156a
Ralf Hoffmann <ralf@boomerangsworld.de>
2019-05-26 10:15:24 +0200

    remove LD_PRELOAD support

--- preload/server.h
+++
@@ -1,9 +0,0 @@
-/*
-    AVFS: A Virtual File System Library
-    Copyright (C) 1998-2001  Miklos Szeredi <miklos@szeredi.hu>
-
-    This program can be distributed under the terms of the GNU GPL.
-    See the file COPYING.
-*/
-
-int server_init();
